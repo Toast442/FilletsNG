e1cab0b664adcfed1066a252369c80737169e6c3
Jason Kingan <jasonk@toast442.org>
2013-02-23 20:10:31 -0700

    Upadted make-bundle script to take a data directory
    Now can be run like:
    
    bash$ ./macfiles/make-bundle.sh 0.9.0 /path/to/fillets-ng-data

--- macfiles/make-bundle.sh
+++ macfiles/make-bundle.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
+rm -rf Fillets.app
+mkdir -p Fillets.app/Contents/MacOS
+mkdir -p Fillets.app/Contents/Resources/fillets/share/games/fillets-ng
+
 cat macfiles/Info.plist | sed -e "s/@VERSION@/${1}/" > Fillets.app/Contents/Info.plist
 
+cp src/game/fillets Fillets.app/Contents/MacOS
 cp macfiles/PkgInfo Fillets.app/Contents
 cp macfiles/Fillets.icns Fillets.app/Contents/Resources
+if [ "$2" != "" ]
+then
+    cp -a "$2/"* Fillets.app/Contents/Resources/fillets/share/games/fillets-ng
+    shift
+fi
 
 cd Fillets.app/Contents/MacOS
 
